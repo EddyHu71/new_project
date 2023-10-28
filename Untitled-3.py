dc17a8803c442a077f15435247ccbce996352083
Eddy <eddyhu71@gmail.com>
2023-10-28 16:15:13 +0700

    Init project

---
+++ Untitled-3.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+begin_fill()
+color("red")
+forward(100)
+left(90)
+color("yellow")
+forward(100)
+left(90)
+color("green")
+forward(100)
+end_fill()
+exitonclick()
---
+++ calculator.py
@@ -0,0 +1,5 @@
+from calculation import *
+
+print(tambah(2, 3))
+
+print(kali(3, 3))
